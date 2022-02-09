085b1006e897b1e89211a75e8edae1c1c38cc2cd
Stephen Fong <fongchinghinunsw@gmail.com>
2022-02-09 23:13:33 +0800

    add blockchain introduction page

---
+++ pages/tutorials/blockchain/introduction.tsx
@@ -0,0 +1,14 @@
+import {
+  Title,
+  Subtitle1,
+  Subtitle2,
+  Paragraph,
+  Hyperlink,
+} from "../../../components/Text/Text";
+import Tutorial from "../../../components/Tutorial/Tutorial";
+
+const Introduction = () => {
+  return <Tutorial title="Introduction"></Tutorial>;
+};
+
+export default Introduction;
--- pages/tutorials/react/index.tsx
+++ pages/tutorials/react/index.tsx
@@ -3,6 +3,34 @@ import ChapterList from "../../../components/Tutorial/ChapterList";
 import { Fragment } from "react";
 import styles from "./index.module.scss";
 
+const chapters = [
+  {
+    name: "Setup",
+    path: "/tutorials/react/setup",
+  },
+  {
+    name: "Introduction",
+    path: "/tutorials/react/introduction",
+  },
+  { name: "JSX", path: "/tutorials/react/JSX" },
+  {
+    name: "JSX Deep Dive",
+    path: "/tutorials/react/JSX-deep-dive",
+  },
+  {
+    name: "Function Component",
+    path: "/tutorials/react/function-component",
+  },
+  {
+    name: "Props",
+    path: "/tutorials/react/props",
+  },
+  {
+    name: "State",
+    path: "/tutorials/react/state",
+  },
+];
+
 const ReactTutorialHomeMainSection = () => {
   const introductionChapterList = [
     {
@@ -13,18 +41,6 @@ const ReactTutorialHomeMainSection = () => {
       name: "Introduction",
       path: "/tutorials/react/introduction",
     },
-    // {
-    //   name: "Conditional Rendering",
-    //   path: "/tutorials/react/conditional-rendering",
-    // },
-    // {
-    //   name: "Events",
-    //   path: "/tutorials/react/events",
-    // },
-    // {
-    //   name: "Styling",
-    //   path: "/tutorials/react/styling",
-    // },
   ];
 
   const functionComponentChapterList = [
@@ -42,45 +58,12 @@ const ReactTutorialHomeMainSection = () => {
     },
   ];
 
-  const propsAndStatesChapterList = [
-    {
-      name: "Props",
-      path: "/tutorials/react/props",
-    },
-    {
-      name: "State",
-      path: "/tutorials/react/state",
-    },
-  ];
-
-  const intermediaryChapterList = [
-    {
-      name: "List",
-      path: "/tutorials/react/list",
-    },
-    {
-      name: "Lifting State Up",
-      path: "/tutorials/react/lifting-state-up",
-    },
-    {
-      name: "Forms",
-      path: "/tutorials/react/forms",
-    },
-    {
-      name: "Composition vs Inheritance",
-      path: "/tutorials/react/composition-vs-inheritance",
-    },
-  ];
   return (
     <Fragment>
       <ChapterList title={"Introduction"} chapters={introductionChapterList} />
       <ChapterList
         title={"Components"}
         chapters={functionComponentChapterList}
-      />
-      <ChapterList
-        title={"Props & States"}
-        chapters={introductionChapterList}
       />
       <ChapterList
         title={"Props & States"}
