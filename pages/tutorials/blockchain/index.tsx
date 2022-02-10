d0b955b9fe3477ab7de23d3bca3e27ad493049f0
Stephen Fong <fongchinghinunsw@gmail.com>
2022-02-10 14:48:17 +0800

    fix props input for blockchain home page

--- pages/tutorials/blockchain/index.tsx
+++ pages/tutorials/blockchain/index.tsx
@@ -1,7 +1,21 @@
 import TutorialHome from "../../../components/Tutorial/TutorialHome";
+import usePath, { usePathProps } from "../../../hooks/usePath";
+
+let chapters: usePathProps = [
+  {
+    sectionName: "Introduction",
+    subSections: [
+      {
+        name: "Introduction",
+        path: "",
+      },
+    ],
+  },
+];
 
 const BlockchainTutorialHome = () => {
-  return <TutorialHome title="Blockchain"></TutorialHome>;
+  chapters = usePath(chapters);
+  return <TutorialHome title="Blockchain" chapters={chapters}></TutorialHome>;
 };
 
 export default BlockchainTutorialHome;
