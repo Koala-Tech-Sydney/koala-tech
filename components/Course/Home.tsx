b8f9e0890c27458e23348dd474d03fb62e6eae3f
Stephen Fong <fongchinghinunsw@gmail.com>
2022-03-10 20:37:17 +0800

    [refactor] extract UnitAccordian component

--- components/Course/Home.tsx
+++ components/Course/Home.tsx
@@ -1,9 +1,8 @@
-import { Fragment } from "react";
 import Link from "next/link";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import TwoSidesMainSection from "../Structure/TwoSidesMainSection";
 import SearchBar from "./SearchBar";
@@ -11,8 +10,7 @@ import { Title, Paragraph } from "../Text/Text";
 import { Course, Unit, Chapter } from "../../hooks/useCourse";
 import SideNavBar from "./SideNavBar";
 
-import styles from './Home.module.scss'
-
+import styles from "./Home.module.scss";
 
 type Props = {
   title: string;
@@ -23,73 +21,75 @@ const CourseHome: React.FC<Props> = ({ title, course }) => {
     <TwoSidesMainSection
       leftSection={<SideNavBar course={course} />}
       mainSection={
-        <Fragment>
+        <>
           <Title>{title}</Title>
           <Paragraph>{course.description}</Paragraph>
-          <br /><hr /><br />
-
+          <hr />
+          <br />
           <SearchBar course={course} />
-
-          <div className={styles.unitListContainer}>
-            <UnitList units={course.units} />
-          </div>
-
-        </Fragment>
+          <br />
+          <UnitList units={course.units} />
+        </>
       }
       rightSection={<div></div>}
     />
   );
 };
 
+type UnitListProps = { units: Unit[] };
+const UnitList: React.FC<UnitListProps> = ({ units }) => {
+  return (
+    <>
+      {units.map((unit) => {
+        return <UnitAccordian key={unit.id} unit={unit} />;
+      })}
+    </>
+  );
+};
 
-type UnitListProps = {units: Unit[];}
-const UnitList: React.FC<UnitListProps> = ({units}) => {
+type UnitAccordianProps = {
+  unit: Unit
+}
+const UnitAccordian: React.FC<UnitAccordianProps> = ({unit}) => {
+  return (
+    <Accordion className={styles.kAccordion}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon className={styles.expandIcon} />}
+      >
+        <div>
+          <div>{unit.name}</div>
+          <div className={styles.chapterNum}>
+            {unit.chapters.length} chapters (
+            {getTotalRequiredReadingTimeInMinute(unit.chapters)} mins)
+          </div>
+        </div>
+      </AccordionSummary>
 
-  return(
-    <Fragment>
-      {units.map(unit => {
-        return(
-          <div key={unit.id} className={styles.accordionContainer}>
-            <Accordion className={styles.kAccordion}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon className={styles.expandIcon}/>}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-                className={styles.unitTitle}
-              >
-                <div>
-                  <div>{unit.name} </div>
-                  <div className={styles.chapterNum}>
-                    {unit.chapters.length} chapters 
-                    ({sum(unit.chapters)} mins)
+      <AccordionDetails className={styles.detailContainer}>
+        {unit.chapters.map((chapter) => {
+          return (
+            <Link key={chapter.id} href={chapter.path} passHref>
+              <a>
+                <div className={styles.chContainer}>
+                  <div>{chapter.name}</div>
+                  <div className={styles.chMins}>
+                    {chapter.requiredReadingTimeInMinute} mins
                   </div>
                 </div>
-              </AccordionSummary>
-
-              <AccordionDetails className={styles.detailContainer}>
-                {unit.chapters.map(chapter => {
-                  return <Link key={chapter.id} href={chapter.path} passHref>
-                    <a>
-                      <div className={styles.chContainer}>
-                        <div>{chapter.name}</div>
-                        <div className={styles.chMins}>{chapter.requiredReadingTimeInMinute} mins</div>
-                      </div>
-                    </a>
-                  </Link>                    
-                })}
-              </AccordionDetails>
-            </Accordion>
-          </div>
-        )
+              </a>
+            </Link>
+          );
         })}
+      </AccordionDetails>
+    </Accordion>
+  );
+};
 
-    </Fragment>
-  )
-}
-
-function sum( chList: Chapter[] ) {
+function getTotalRequiredReadingTimeInMinute(chList: Chapter[]) {
   var sum = 0;
-  chList.forEach(ch => {sum += ch.requiredReadingTimeInMinute;})
+  chList.forEach((ch) => {
+    sum += ch.requiredReadingTimeInMinute;
+  });
   return sum;
 }
 
